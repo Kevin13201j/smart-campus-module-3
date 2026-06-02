8e8a176589980c5e8cf1a455f425419176ccd92a
Diego050109 <lemadiego597@gmail.com>
2026-06-01 22:04:39 -0500

    refactor: remove library dto files

--- apps/campus-incident-service/src/application/dto/loan-book.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class LoanBookDto {
-  @ApiProperty({ example: 'student-001' })
-  @IsString()
-  @IsNotEmpty()
-  studentId!: string;
-}
